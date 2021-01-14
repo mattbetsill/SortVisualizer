60bf2127ab7fe0901a8438f9f4537a693dd1d77f
Matthew <matthewb22@vt.edu>
2021-01-14 13:42:52 -0500

    update to comments

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 
 var speed = 25;
+//getter method for height of element
 function getHeight(element){
     return parseFloat(element.style.height); 
 }
-
+//getter method for color of element
 function getColor(element){
     return element.style.backgroundColor;
 }
+//setter method for height of element
 function setHeight(element, height){
     element.style.height = height;
-
-
 }
-
+//setter method to set an element's color to red
 function setRed(element){
     element.style.backgroundColor= "#FF0000";
 }
+//setter method for setting element's color to green
 function setGreen(element){
     element.style.backgroundColor = "#00FF00";
 }
-
+//setter method for setting element's color to white
 function setWhite(element){
     element.style.backgroundColor = "white";
 }
-
-function setHeight(element, height){
-    element.style.height = height;
-}
-
+//getter method for input element value
 function getRectangleNumber(){
     var num = document.getElementById("recInput").value;
-
     return num;
-
 }
-//            <div id="outerelement" style="width:10px;height:100px; solid #000; float:left">
-//                <div class="element" style="width:10px;height:100px;border:1px solid #000; float:left">
-//                </div>
-//            </div>
+//renders the rectangles that represent different values,
+//assigns tooltip functionality and makes elements white
 function renderRectangles(){
     var num = getRectangleNumber();
     document.getElementById("rectangles").innerHTML = "";
@@ -52,41 +45,40 @@ function renderRectangles(){
     }
     assignHoverRectangle();
     assignHeight();
-
-
-
-
+    makeAllWhite(document.getElementsByClassName("element"));
 }
+//eventlistener for the enter button
 document.getElementById("enterRectangleNum").addEventListener('click', renderRectangles);
-var elements = document.getElementsByClassName("element");
+//setter method for setting random heights to rectangles
 function setHeights(elementList){
     for (var i = 0; i < elementList.length; i++){
         setHeight(elementList[i], (Math.random() * 50) + 10);
     }
-
 }
 
 var slider = document.getElementById("myRange");
+//setter method for speed of functionality determined by slider
 slider.oninput = function(){
 
     speed = 1 + 100 - this.value;
 }
 // Returns a Promise that resolves after "ms" Milliseconds
 var timer = ms => new Promise(res => setTimeout(res, ms))
-
-async function load (time) { // We need to wrap the loop into an async function for this to work
+//loads for the amount of time in ms, then iterates loop 
+async function load (time) { 
 
     for (var i = 0; i < speed; i++) {
 
-        await timer(time); // then the created Promise can be awaited
+        await timer(time); 
     }
 }
-
+//setter method makes all elements in the elements list white
 function makeAllWhite(elements){
     for (var i = 0; i < elements.length; i++){
         setWhite(elements[i]);
     }
 }
+//adds eventlistener to randomize button
 document.getElementById("randomize").addEventListener('click', function(){
     var elem = document.getElementsByClassName("element");
     makeAllWhite(elem);
@@ -104,6 +96,7 @@ function my_Clock()
     this.miliseconds = 0;
     this.interval;
 }
+//function to run the clock, setting variables to zero
 my_Clock.prototype.run = function ()
 {
     this.hours = 0;
@@ -113,6 +106,7 @@ my_Clock.prototype.run = function ()
     this.interval;
     this.interval = setInterval(this.update.bind(this), 10);
 };
+//updates the screen to display the minutes, sectonds, and hours when necessary
 my_Clock.prototype.update = function () 
 {
     this.updateTime(1);
@@ -126,6 +120,7 @@ my_Clock.prototype.update = function ()
         document.getElementById("timer").innerHTML = (this.seconds + "." + this.miliseconds + " s");
     }
 };
+//updates the time every milisecond
 my_Clock.prototype.updateTime = function (miliseconds) 
 {
 
@@ -149,12 +144,13 @@ my_Clock.prototype.updateTime = function (miliseconds)
         this.hours = 0;
     }
 };
-
+//stops the timer
 my_Clock.prototype.stopInterval = function(){
     clearInterval(this.interval);
 };
 var clock = new my_Clock();
 ////////////////////////////////////////////////////////////////////
+//renders a default value of 25 rectangles onto the screen on load
 window.onload = function(){
     var num = 25
     document.getElementById("rectangles").innerHTML = "";
@@ -192,11 +188,15 @@ function enableSorts(){
 function stayWhite(button){
     button.setAttribute("style", "background-color: white; color:black;");
 }
-
+//sets the color of the elements back to black background, white text
 function turnBack(button){
     button.setAttribute("style", "background-color: black; color:white;");
 }
 ////////////////////////////////////////////////////////////////////
+/*selection sort algorithm updates the rectangle heights when necessary
+parses all values, finds largest, puts at end of list, then swaps the place of the 
+largest value it found with the place the largest value will be placed
+*/
 var selectionSort = async function(){
     disableSorts();
     clock.run();
@@ -215,19 +215,14 @@ var selectionSort = async function(){
                 max = getHeight(rects[i]);
                 maxIndex = i;
                 oldHeight = getHeight(rects[i]);
-
-
             }
             else{
                 oldHeight = getHeight(rects[i]);
                 setHeight(rects[i], max);
-
             }
             setRed(rects[i]);
-
             await load(1);
             setWhite(rects[i]);
-
             if (innerLim == i + 1){
                 setHeight(rects[maxIndex], oldHeight); 
                 innerLim--;
@@ -235,17 +230,10 @@ var selectionSort = async function(){
                 maxIndex = 0;
                 oldHeight = 0;
                 setGreen(rects[i]);
-
-
             }else{
                 setHeight(rects[i], oldHeight);
             }
-
         }
-
-
-
-
     }
     clock.stopInterval();
     turnBack(button);
@@ -253,6 +241,10 @@ var selectionSort = async function(){
     speed = 1 + 100 - slider.value;
 }
 //////////////////////////////////////////////////////////////////////
+/*insertion sort algorithm places the next element in the correct place
+by pushing larger elements forward by one index, creating space for
+the new value in the sorted portion of the list
+*/
 var insertionSort = async function(){
     disableSorts();
     var button = document.getElementById("insertionSort");
@@ -273,6 +265,8 @@ var insertionSort = async function(){
                 wasGreen = true;
             }
             setHeight(rects[j+1], getHeight(rects[j]));
+            var oldheight = getHeight(rects[j]);
+            setHeight(rects[j], heightToPlace);
             setRed(rects[j]);
             await load(1);
             if (wasGreen){
@@ -280,35 +274,22 @@ var insertionSort = async function(){
             }else{
                 setWhite(rects[j]);
             }
-
+            setHeight(rects[j], oldheight);
             j = j - 1;
         }
         setHeight(rects[j+1], heightToPlace);
         setGreen(rects[j+1]);
         setGreen(rects[o]);
     }
-
-
-    //            
-    //
-    //            setRed(rects[i]);
-    //
-    //            
-    //            setWhite(rects[i]);
-    //            if (heightToPlace > getHeight(rects[i]) || i==0){
-    //                setHeight(rects[i], heightToPlace);
-    //                //alert("here " + heightToPlace + " " + i + " " + o );
-    //                i = -1;
-    //                heightToPlace = 0;
-    //                }
-    //            }
-
     clock.stopInterval();
     turnBack(button);
     enableSorts();
     speed = 1 + 100 - slider.value;
 }
 //////////////////////////////////////////////////////////////////////
+/*bubble sort swaps each element if they are out of order,
+iterates over the length of the list multiple times
+*/
 var bubbleSort = async function(){
     disableSorts();
     var button = document.getElementById("bubbleSort");
@@ -322,14 +303,11 @@ var bubbleSort = async function(){
     do{
         swapped = false;
         let i = 0;
-        for(i = 0; i < len; i++){
+        for(i = 0; i < len - sorted; i++){
             let wasGreen = false;
-
             if (getColor(rects[i + 1]).toString().localeCompare("rgb(0, 255, 0)")==0){
                 wasGreen = true;
-
             }
-
             setRed(rects[i + 1]);
             await load(1);
             setWhite(rects[i + 1]);
@@ -338,27 +316,17 @@ var bubbleSort = async function(){
             }else{
                 setWhite(rects[i + 1]);
             }
-
             if (getHeight(rects[i]) > getHeight(rects[i+1])){
-
                 let temp = getHeight(rects[i]);
                 setHeight(rects[i], getHeight(rects[i + 1]));
                 setHeight(rects[i + 1], temp);
                 swapped = true;
-
-
-
             }
-            last = i + 1 - sorted;
-
-
+            last = i + 1;
         }
         setGreen(rects[last]);
-
-
         sorted++;
     }while (swapped);
-
     for(var i = 0; i <= len - sorted; i++){
         await load(1);
         setGreen(rects[i]);
@@ -369,6 +337,9 @@ var bubbleSort = async function(){
     speed = 1 + 100 - slider.value;
 }
 /////////////////////////////////////////////////////////////////////
+/*quicksort algorithm requires helper methods swap and partition
+swqp takes the elements at the two indexes and swaps them
+*/
 async function swap(items, leftIndex, rightIndex){
     var temp = getHeight(items[leftIndex]);
     setGreen(items[leftIndex]);
@@ -382,15 +353,17 @@ async function swap(items, leftIndex, rightIndex){
     console.log("here");
 
 }
+/*partition finds the index near the middle of the list, 
+then walks down from the right and left towards the middle,
+swapping values that are greater and less than the pivot
+*/
 async function partition(items, left, right) {
     var pivot = getHeight(items[Math.floor((right + left) / 2)]), 
         //middle element
         i = left, //left pointer
         j = right; //right pointer
     while (i <= j) {
-
         while (getHeight(items[i]) < pivot) {
-
             i++;
         }
         while (getHeight(items[j]) > pivot) {
@@ -404,14 +377,11 @@ async function partition(items, left, right) {
         if (i >=0 && j >= 0){
             let wasIGreen = false;
             let wasJGreen = false;
-
             if (getColor(items[i]).toString().localeCompare("rgb(0, 255, 0)")==0){
                 wasIGreen = true;
-
             }
             if (getColor(items[j]).toString().localeCompare("rgb(0, 255, 0)")==0){
                 wasJGreen = true;
-
             }
             setRed(items[i]);
             setRed(items[j]);
@@ -429,37 +399,29 @@ async function partition(items, left, right) {
                 setWhite(items[j]);
             }
         }
-
-
     }
     setGreen(items[i - 1]);
-
     return i;
 }
-
+/*quicksort recursive algorithm drives the partition algorithm
+to analyze different lists
+*/
 async function quickSortHelper(items, left, right) {
     var index;
-
     if (items.length > 1) {
         index = await partition(items, left, right); //index returned from partition
-
         if (left < index - 1) { //more elements on the left side of the pivot
             await quickSortHelper(items, left, index - 1);
         }
         if (index < right) { //more elements on the right side of the pivot
             await quickSortHelper(items, index, right);
         }
-
     }
-
-
     return items;
-
-
 }
 
 
-
+//quicksort method that utilizes the quickSortHelper
 var quickSort = async function(){
     var rects = document.getElementsByClassName("element");
     var button = document.getElementById("quickSort");
@@ -479,7 +441,9 @@ var quickSort = async function(){
 
 }
 /////////////////////////////////////////////////////////////////////////
-
+/*merge function takes the greatest element from the two
+lists and pushes it into the new array
+*/
 function merge(left, right) {
     let arr = [];
 
@@ -494,7 +458,8 @@ function merge(left, right) {
 
     return arr.concat(left.slice().concat(right.slice()));
 }
-
+/*splits the array until the length of the arrays is less than 2
+*/
 function mergeSortHelper(arr) {
     if (arr.length < 2) {
         return arr;
@@ -509,7 +474,7 @@ function mergeSortHelper(arr) {
 
 
 
-
+//mergesort function
 var mergeSort = function(){
     var rects = document.getElementsByClassName("element");
     var button = document.getElementById("mergeSort");
@@ -531,9 +496,137 @@ var mergeSort = function(){
     speed = 1 + 100 - slider.value;
 }
 
+////////////////////////////////////////////////////////////////////////
+//merge sort v2
+function sortMerge(arr1, arr2, len1, len2){
+    let i = len1 - 1;
+    let j = len2 - 1;
+    let last = len1 + len2 - 1;
+    
+    while (j >= 0){
+        if (i >= 0 && arr1[i] > arr2[i]){
+            arr1[last] = arr1[i];
+            i--;
+        }
+        else{
+            arr1[last] = arr2[j];
+            j--;
+        }
+        last--;
+    }
+    return arr1;
+}
+
+var mergeSort2 = function(){
+    var rects = document.getElementsByClassName("element");
+    var button = document.getElementById("mergeSort");
+    stayWhite(button);
+    var arr = [];
+    var arr2 = [];
+    var index2 = 0;
+    for (var i = 0; i < rects.length; i++){
+        if (i < Math.floor(rects.length / 2)){
+        arr[i] = getHeight(rects[i]);
+        }else{
+            arr[i] = -1;
+        }
+                
+            
+    }
+    
+    for (var i = Math.floor(rects.length/2); i < rects.length; i++){
+       
+        arr2[index2] = getHeight(rects[i]);
+        index2++;
+    }
+
+    disableSorts();
+    var final = sortMerge(arr, arr2, Math.floor(rects.length/2) - 1, index2 - 1);
+
+    for (var i = 0; i < final.length; i++){
+        setHeight(rects[i], (final[i]));
+
+    }
+    turnBack(button);
+    enableSorts();
+    speed = 1 + 100 - slider.value;
+}
+
+
+
 
 ////////////////////////////////////////////////////////////////////////
+//mergesortv3
+async function merge3(left, right) {
+    let arr = [];
+    right = Array.from(right);
+    left = Array.from(left)
+    while (left.length && right.length) {
+        if (getHeight(left[0]) < getHeight(right[0])) {
+            arr.push(left.shift());
+            await load(1);
+            
+            
+        } else {
+            arr.push(right.shift());
+            await load(1);
+            
+        }
+    }
+    
+    return arr.concat(left.concat(right));
+}
 
+ async function mergeSortHelper3(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    let count = 0;
+    let middle = Math.floor(arr.length / 2);
+    let left = [];
+    let right =  [];
+    for (var i = 0; i < middle; i++){
+        left[i] = arr[i];
+    }
+     for (var i = middle; i < arr.length; i++){
+         right[count] = arr[i];
+         count++;
+     }
+     return await merge3(await mergeSortHelper3(left), await mergeSortHelper3(right));
+     
+    
+}
+
+function redraw(rects, elements){
+    var rects = document.getElementsByClassName("element");
+    for (var i = 0; i < elements.length; i++){
+        setHeight(rects[i], getHeight(elements[i]));
+        
+    }
+}
+
+
+var mergeSortWithHeight = async function(){
+    var rects = document.getElementsByClassName("element");
+    var button = document.getElementById("mergeSort");
+    stayWhite(button);
+
+    
+   
+    disableSorts();
+    
+    var rectsFinal = await mergeSortHelper3(Array.from(rects));
+    for (var i = 0; i < rectsFinal.length; i++){
+        console.log(getHeight(rectsFinal[i]));
+    }
+   redraw(rects, rectsFinal);
+  
+    turnBack(button);
+    enableSorts();
+    speed = 1 + 100 - slider.value;
+}
+////////////////////////////////////////////////////////////////////////
+//rectangle tooltips
 
 var rectHover = function(rect){
 
@@ -554,21 +647,22 @@ var onHover = function(){
 }
 
 ///////////////////////////////////////////////////////////
-
+//setter function sets the speed to zero
 var setSpeedZero = function(){
     speed = 0;
     clock.stopInterval();
 
 }
 /////////////////////////////////////////////////////////////////////
-assignFunctionalityInsertion();
+//assigning events
+
 function assignFunctionalityInsertion(){
 
     var insertionButton = document.getElementById("insertionSort");
 
     insertionButton.addEventListener('click', insertionSort);
 }
-assignFunctionalitySelection();
+
 function assignFunctionalitySelection(){
     var selectionButton = document.getElementById("selectionSort");
     selectionButton.addEventListener('click', selectionSort);
@@ -580,20 +674,20 @@ function assignFunctionalityBubble(){
 
     bubbleButton.addEventListener('click', bubbleSort);
 }
-assignFunctionalityBubble();
+
 
 function assignFunctionalityQuick(){
     var quickButton = document.getElementById("quickSort");
     quickButton.addEventListener('click', quickSort);
 }
-assignFunctionalityQuick();
+
 
 function assignFunctionalityMerge(){
     var mergeButton = document.getElementById("mergeSort");
 
-    mergeButton.addEventListener('click', mergeSort);
+    mergeButton.addEventListener('click', mergeSortWithHeight);
 }
-assignFunctionalityMerge();
+
 function assignHoverRectangle(){
     var rects = document.getElementsByClassName("element");
     for (var i = 0; i < rects.length; i++){
@@ -625,10 +719,11 @@ function assignFinishSort(){
 assignFinishSort();
 assignHoverRectangle();
 assignHeight();
-
-
-
-
+assignFunctionalitySelection();
+assignFunctionalityInsertion();
+assignFunctionalityBubble();
+assignFunctionalityQuick();
+assignFunctionalityMerge();
 /////////////////////////////////////////////////////////////////////
 
 
